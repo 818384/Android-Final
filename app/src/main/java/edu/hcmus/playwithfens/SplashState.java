2e096c2f06252563c340fee87b91859c4d012434
18424081 <18424081@studen.hcmus.edu.vn>
2020-08-29 18:22:29 +0700

    delay time for splash state

--- app/src/main/java/edu/hcmus/playwithfens/SplashState.java
+++ app/src/main/java/edu/hcmus/playwithfens/SplashState.java
@@ -1,7 +1,9 @@
 package edu.hcmus.playwithfens;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,5 +26,14 @@ public class SplashState extends Activity {
         nameGame = (TextView) findViewById(R.id.nameGame);
 
         logo.setImageResource(R.drawable.splash_image);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashState.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
