aed7072706bafb62e03966e96db7afd921b2bf3b
Vu Pham <18424084@student.hcmus.edu.vn>
2020-09-27 11:30:34 +0700

    Background with fishes in the middle

--- app/src/main/java/edu/hcmus/playwithfens/GameView.java
+++ app/src/main/java/edu/hcmus/playwithfens/GameView.java
@@ -137,10 +137,10 @@ public class GameView extends SurfaceView implements Runnable {
         // Khởi tạo màn khởi đầu.
         background = BitmapFactory.decodeResource(getResources(), R.drawable.bg_final, option);
         background2 = BitmapFactory.decodeResource(getResources(), R.drawable.bg_firewall, option);
-        backgroundWinner = background2;
-//        backgroundWinner = BitmapFactory.decodeResource(getResources(), R.drawable.winner, option);
-        backgroundLoser = background2;
-//        backgroundLoser = BitmapFactory.decodeResource(getResources(), R.drawable.loser, option);
+//        backgroundWinner = background2;
+        backgroundWinner = BitmapFactory.decodeResource(getResources(), R.drawable.winner, option);
+//        backgroundLoser = background2;
+        backgroundLoser = BitmapFactory.decodeResource(getResources(), R.drawable.loser, option);
         backgroundGame = new GameObject(0, 0, background);
 
         // Rocket.
